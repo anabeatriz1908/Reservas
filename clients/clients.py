5b5d598b54c3dbe08c08e4e800cf4a79710162d5
Luiz <luizotaviossilva@hotmail.com>
2025-05-26 18:25:27 -0300

    Fix: Editando clients para a entidade turma

--- clients/clients.py
+++ clients/clients.py
@@ -4,14 +4,13 @@ API_PRINCIPAL_URL = "http://localhost:5036"
 
 class PessoaServiceClient:
     @staticmethod
-    def verificar_leciona(id_professor):
-        url = f"{API_PRINCIPAL_URL}/professores/{id_professor}"
+    def verificar_turma(id_turma):
+        url = f"{API_PRINCIPAL_URL}/turmas/{id_turma}"
         try:
             response = requests.get(url)
             response.raise_for_status()
             data = response.json()
-            return data.get('professores', False)
-            #if data.get('isok') else False
+            return data.get('turmas', False)
         except requests.RequestException as e:
             print(f"Erro ao acessar o pessoa_service: {e}")
             return False
